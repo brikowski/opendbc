17e1f614d8b394147ba439722cb11aad396d4c66
Travis Badgley <tbadgley@hotmail.com>
2026-08-28 21:58:55 -0500

    Honda: test nonlinear Odyssey steering range

--- opendbc/car/honda/tests/test_honda.py
+++ opendbc/car/honda/tests/test_honda.py
@@ -1,5 +1,7 @@
 import unittest
 
+from opendbc.car import gen_empty_fingerprint
+from opendbc.car.honda.interface import CarInterface
 from opendbc.car.honda.values import CAR, HondaFlags
 
 
@@ -8,3 +10,12 @@ class TestHondaFingerprint(unittest.TestCase):
     for car_model in CAR:
       if car_model.config.flags & HondaFlags.BOSCH_TJA_CONTROL:
         assert car_model.config.flags & HondaFlags.BOSCH, "Nidec car found with TJA control"
+
+
+class TestHondaParams(unittest.TestCase):
+  def test_odyssey_mmr_lateral_range(self):
+    for alpha_long in (False, True):
+      CP = CarInterface.get_params(CAR.HONDA_ODYSSEY_5G_MMR, gen_empty_fingerprint(), [], alpha_long, False, False)
+      assert list(CP.lateralParams.torqueBP) == [0, 2560, 3072]
+      assert list(CP.lateralParams.torqueV) == [0, 2560, 3840]
+      assert CP.openpilotLongitudinalControl == alpha_long
