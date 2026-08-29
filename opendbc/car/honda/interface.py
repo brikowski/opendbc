659b466e25114580c2efabb6bd655fca9833e8cc
Travis Badgley <tbadgley@hotmail.com>
2026-08-29 17:11:33 -0500

    Honda: restore stock Odyssey steering range

--- opendbc/car/honda/interface.py
+++ opendbc/car/honda/interface.py
@@ -182,8 +182,8 @@ class CarInterface(CarInterfaceBase):
       ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.6], [0.18]] # TODO: can probably use some tuning
 
     elif candidate == CAR.HONDA_ODYSSEY_5G_MMR:
-      # Motor torque rises nonlinearly above the stock LKA range.
-      ret.lateralParams.torqueBP, ret.lateralParams.torqueV = [[0, 2560, 3072], [0, 2560, 3840]]
+      # Use the stock LKA range; RDM's higher range relies on separate braking we do not command.
+      ret.lateralParams.torqueBP, ret.lateralParams.torqueV = [[0, 2560], [0, 2560]]
       CarInterfaceBase.configure_torque_tune(candidate, ret.lateralTuning)
       ret.steerActuatorDelay = 0.15
       if not ret.openpilotLongitudinalControl:
