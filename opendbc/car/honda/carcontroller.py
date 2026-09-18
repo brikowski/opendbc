409c25925c19396bc58f72f00fcc5b2b61a795d9
Travis Badgley <tbadgley@hotmail.com>
2026-09-18 13:06:55 -0500

    honda: release Odyssey gas at response boundary

--- opendbc/car/honda/carcontroller.py
+++ opendbc/car/honda/carcontroller.py
@@ -13,6 +13,7 @@ LongCtrlState = structs.CarControl.Actuators.LongControlState
 ODYSSEY_LOW_SPEED_DOMAIN_VEGO = 5.0
 # Keep mild negative road-speed requests in Honda's neutral coast domain; stronger requests retain
 # immediate friction-brake authority. Domain selection remains based on the raw controller request.
+ODYSSEY_ROAD_GAS_RELEASE = -0.15
 ODYSSEY_ROAD_BRAKE_ENTRY = -0.30
 
 
@@ -20,7 +21,7 @@ def odyssey_command_domains(accel, speed, previous_brake=False, previous_gas=Fal
   """Keep low-speed stop authority and separate road-speed coast from friction braking."""
   gas_selected = accel > 0.0
   if speed >= ODYSSEY_LOW_SPEED_DOMAIN_VEGO:
-    gas_selected |= previous_gas and accel > CarControllerParams.BOSCH_GAS_LOOKUP_BP[0]
+    gas_selected |= previous_gas and accel > ODYSSEY_ROAD_GAS_RELEASE
     brake_selected = accel < ODYSSEY_ROAD_BRAKE_ENTRY or (previous_brake and accel < 0.0)
   else:
     brake_selected = accel <= 0.0
